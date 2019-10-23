5a106d6a33e7e6e348f44ad6c763bfbbf7ffde23
francismaria <franciscomaria97@hotmail.com>
2019-10-23 17:53:48 +0100

    Add login action to communicate with the server and add style to login page

---
+++ Acme/app/src/main/java/feup/mieic/cmov/acme/connect/LoginAction.java
@@ -0,0 +1,9 @@
+package feup.mieic.cmov.acme.connect;
+
+public class LoginAction implements Runnable {
+
+    @Override
+    public void run(){
+        System.out.println("INVOKEED------------------------------------");
+    }
+}
