8d313d90bbaf7e53fad966804fb2b3e9fa985b4c
francismaria <franciscomaria97@hotmail.com>
2019-10-25 11:01:40 +0100

    Add email validator to register screen

--- Acme/app/src/main/java/feup/mieic/cmov/acme/RegisterActivity.java
+++ Acme/app/src/main/java/feup/mieic/cmov/acme/RegisterActivity.java
@@ -4,19 +4,30 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.net.URL;
+
+import feup.mieic.cmov.acme.validation.TextValidator;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private URL registerPath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setSupportActionBarIcon();
+        setInputValidators();
     }
 
-    /**
-     * Customizes the support action bar.
-     */
+    /** -------------------------------
+     *            ACTION BAR
+     *  ------------------------------- */
+
     private void setSupportActionBarIcon(){
         ActionBar bar = getSupportActionBar();
 
@@ -25,5 +36,60 @@ public class RegisterActivity extends AppCompatActivity {
             bar.setDisplayShowHomeEnabled(true);
             bar.setTitle("Register");
         }
+    }
+
+    /** -------------------------------
+     *        INPUT VALIDATORS
+     *  ------------------------------- */
+
+
+    private void setInputValidators(){
+        setEmailValidator((EditText)this.findViewById(R.id.registerEmail));
+        setUsernameValidator((EditText)this.findViewById(R.id.registerUsername));
+        setPasswordValidator((EditText)this.findViewById(R.id.registerPassword));
+        setConfirmPasswordValidator((EditText)this.findViewById(R.id.registerConfirmPassword));
+        setCreditCardInfoValidator();
+    }
+
+    private void setEmailValidator(EditText emailView){
+        emailView.addTextChangedListener(new TextValidator(emailView) {
+
+            @Override
+            public void validate(TextView view, String text) {
+                if(!Patterns.EMAIL_ADDRESS.matcher(text).matches()){
+                    view.setError("The e-mail format is not accepted.");
+                }
+            }
+        });
+    }
+
+    private void setUsernameValidator(EditText textView){
+
+    }
+
+    private void setPasswordValidator(EditText textView){
+
+    }
+
+    private void setConfirmPasswordValidator(EditText textView){
+
+    }
+
+    private void setCreditCardInfoValidator(){
+        setCreditCardNumberValidator((EditText)this.findViewById(R.id.registerCardNr));
+        setCreditCardExpDateValidator((EditText)this.findViewById(R.id.registerExpDate));
+        setCreditCardCCVvalidator((EditText)this.findViewById(R.id.registerCCV));
+    }
+
+    private void setCreditCardNumberValidator(EditText textView){
+
+    }
+
+    private void setCreditCardExpDateValidator(EditText textView){
+
+    }
+
+    private void setCreditCardCCVvalidator(EditText textView){
+
     }
 }
