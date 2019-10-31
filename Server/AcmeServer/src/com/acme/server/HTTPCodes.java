406cbc3939b24350582eaae0cb8ec0800a5d58c9
francismaria <franciscomaria97@hotmail.com>
2019-10-31 12:09:17 +0000

    Change URL pattern

--- Server/AcmeServer/src/com/acme/server/HTTPCodes.java
+++ Server/AcmeServer/src/com/acme/server/HTTPCodes.java
@@ -3,7 +3,10 @@ package com.acme.server;
 public interface HTTPCodes {
 
 	public static final int SUCCESS_CODE = 200;
+	
 	public static final int UNAUTHORIZED_CODE = 401;
+	
 	public static final int FORBIDDEN_CODE = 403;
+	
 	public static final int INTERNAL_SERVER_ERROR_CODE = 500;
 }
