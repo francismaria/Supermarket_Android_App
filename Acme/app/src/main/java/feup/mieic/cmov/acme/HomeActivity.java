f7c3b9a42b2e34c9eeec9c23946bfde2aefbac77
francismaria <franciscomaria97@hotmail.com>
2019-11-05 09:08:34 +0000

    Add home fragment to drawer

--- Acme/app/src/main/java/feup/mieic/cmov/acme/HomeActivity.java
+++ Acme/app/src/main/java/feup/mieic/cmov/acme/HomeActivity.java
@@ -36,8 +36,8 @@ public class HomeActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_profile, R.id.nav_history, R.id.nav_vouchers,
-                R.id.nav_settings, R.id.nav_contact, R.id.nav_help)
+                R.id.nav_home, R.id.nav_profile, R.id.nav_history,
+                R.id.nav_vouchers, R.id.nav_settings, R.id.nav_contact)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
