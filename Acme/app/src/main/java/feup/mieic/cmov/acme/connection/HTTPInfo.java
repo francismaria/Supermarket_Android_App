0876b5236b7bad507a1c0480853f98614320f9c8
francismaria <franciscomaria97@hotmail.com>
2019-11-06 20:16:39 +0000

    Finish profile style

--- Acme/app/src/main/java/feup/mieic/cmov/acme/connection/HTTPInfo.java
+++ Acme/app/src/main/java/feup/mieic/cmov/acme/connection/HTTPInfo.java
@@ -6,9 +6,14 @@ package feup.mieic.cmov.acme.connection;
 public interface HTTPInfo {
 
     String LOGIN_PATH = "http://10.0.2.2:8080/AcmeServer/api/login";
+
     String REGISTER_PATH = "http://10.0.2.2:8080/AcmeServer/api/register";
 
     // HTTP response codes
+
     int SUCCESS_CODE = 200;
+
+    int BAD_REQUEST = 401;
+
     int UNAUTHORIZED_CODE = 401;
 }
