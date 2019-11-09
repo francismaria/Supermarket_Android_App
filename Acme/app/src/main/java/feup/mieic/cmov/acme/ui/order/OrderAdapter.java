167e5518d8398a2356cda8ae7d11283b467b3eb3
francismaria <franciscomaria97@hotmail.com>
2019-11-09 23:57:46 +0000

    Change action bar title when in order information fragment

--- Acme/app/src/main/java/feup/mieic/cmov/acme/ui/order/OrderAdapter.java
+++ Acme/app/src/main/java/feup/mieic/cmov/acme/ui/order/OrderAdapter.java
@@ -1,7 +1,5 @@
 package feup.mieic.cmov.acme.ui.order;
 
-
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
--- Acme/app/src/main/java/feup/mieic/cmov/acme/ui/order/OrderFragment.java
+++ Acme/app/src/main/java/feup/mieic/cmov/acme/ui/order/OrderFragment.java
@@ -1,8 +1,11 @@
 package feup.mieic.cmov.acme.ui.order;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
@@ -11,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -33,11 +37,24 @@ public class OrderFragment extends Fragment {
         errorToast.show();
     }
 
+    private void initActionBar(){
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Purchase Information");
+    }
+
+
+
+    /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+
+    }*/
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final int orderID;
 
         initToast();
+        initActionBar();
+
         Bundle bundle = this.getArguments();
 
         if (bundle != null) {
